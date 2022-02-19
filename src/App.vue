9bc4805d5fbaaf6963a96edf3d32300451864b66
student <you@example.com>
2022-02-19 15:51:25 +0300

    начал делать сайт

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+
+
+ <div>
+<navbar/> 
+<carusel/>
+ </div>
+  
+</template>
+
+<script>
+import navbar from '@/components/navbar';
+import carusel from '@/components/carusel';
+
+export default {
+  name: 'App',
+  components: {
+    navbar,
+    carusel
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/carusel.vue
@@ -0,0 +1,44 @@
+<template>
+<div id="car1" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://avatars.mds.yandex.net/i?id=2a00000179e07b73e74830a1d17774d878db-4255743-images-thumbs&n=13" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</template>
+
+<script>
+
+export default{
+    name: 'carusel'
+
+}
+</script>
+
+<style scope>
+#car1{
+  width: 800px;
+  left: 250px;
+}
+
+</style>
